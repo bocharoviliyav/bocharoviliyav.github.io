b1ce076d5453d1ff972c8d34d955fc7aec5ab98c
Ilia Bocharov <bocharoviliyav@gmail.com>
2021-02-24 23:57:02 +0400

    pwa support was added

--- sw.js
+++ sw.js
@@ -1,37 +1,8 @@
-workbox.core.setCacheNameDetails({
-    prefix: 'YAFDB',
-    suffix: 'v1.0.0',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.endsWith('.png')) {
+        const cacheFirst = new workbox.strategies.CacheFirst();
+        event.respondWith(cacheFirst.handle({request: event.request}));
+    }
 });
-
-workbox.googleAnalytics.initialize();
-
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
-
-workbo.google
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
-
-workbox.routing.registerRoute(
-    /\.html$/,
-    new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
-    new workbox.strategies.NetworkFirst()
-);
-
-workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    new workbox.strategies.CacheFirst()
-);
-
-workbox.routing.registerRoute(
-    /^https?:\/\/fonts\.googleapis\.com/,
-    workbox.strategies.staleWhileRevalidate()
-);
