958b272cd05ab672fd257389705d633aaaacd6a0
Ilia Bocharov <bocharoviliyav@gmail.com>
2021-02-26 14:33:24 +0400

    seo fix

--- sw.js
+++ sw.js
@@ -2,14 +2,8 @@ importScripts('/assets/js/workbox-sw.js');
 
 self.addEventListener('fetch', (event) => {
     if (event.request.url.endsWith('.webp') || event.request.url.endsWith('.css') || event.request.url.endsWith('.webm')) {
-        const cacheFirst = new workbox.strategies.CacheFirst({
-            cacheName: 'image-cache',
-            plugins: [
-                new workbox.expiration.ExpirationPlugin({
-                    maxAgeSeconds: 24 * 60 * 60,
-                }),
-            ],
-        });
-        event.respondWith(cacheFirst.handle({request: event.request}));
+        const cacheFirst = new workbox.strategies.CacheFirst()
+        cacheFirst.cacheName = 'media-and-css'
+        event.respondWith(cacheFirst.handle({request: event.request}))
     }
 });
